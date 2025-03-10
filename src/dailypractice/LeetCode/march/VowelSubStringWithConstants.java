d0135807fe612448765b1b8af25085c95623e669
A-n-b-a-r-a-s-u <anbarasuramsame@gmail.com>
2025-03-10 20:56:33 +0530

    10-03-2025 LC Problems

---
+++ src/dailypractice/LeetCode/march/VowelSubStringWithConstants.java
@@ -0,0 +1,48 @@
+package dailypractice.LeetCode.march;
+
+public class VowelSubStringWithConstants {
+    public static long countOfSubstrings(String word, int k) {
+        long ans = 0;
+        int windowsSize = 5 + k;
+        String temp ;
+        if (windowsSize > word.length()) return 0;
+        for (int i =0; i<= word.length()- windowsSize; i++) {
+            temp = word.substring(i, windowsSize+i);
+            if (temp.contains("a") && temp.contains("e") &&temp.contains("i") && temp.contains("o") && temp.contains("u") ){
+                temp = temp.replace("a", "");
+                temp = temp.replace("e", "");
+                temp = temp.replace("i", "");
+                temp = temp.replace("o", "");
+                temp = temp.replace("u", "");
+            }
+            else {
+                continue;
+            }
+            if (!(temp.contains("a") ||temp.contains("e") ||temp.contains("i") ||
+                    temp.contains("o") || temp.contains("u")) ){
+                ans++;
+            }
+        }
+
+        if (word.contains("a") && word.contains("e") &&word.contains("i") && word.contains("o") && word.contains("u")  ){
+            temp = word;
+            int ct=0;
+            for (int i =0; i< temp.length(); i++){
+                if (temp.charAt(i) == 'q'){
+                    ct++;
+                }
+            }
+            if (ct == k){
+                ans++;
+            }
+
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countOfSubstrings("aeueio",0));
+
+    }
+}
