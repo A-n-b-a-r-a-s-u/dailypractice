4e99989b2bf5fd8c8b620d72812a06bb5d5cf3ca
A-n-b-a-r-a-s-u <anbarasuramsame@gmail.com>
2025-03-10 09:37:45 +0530

    09-03-2025 LC Problem

---
+++ src/dailypractice/LeetCode/march/RecolurConsecutiveBlocks.java
@@ -0,0 +1,26 @@
+package dailypractice.LeetCode.march;
+
+
+public class RecolurConsecutiveBlocks {
+    public static int minimumRecolors(String blocks, int k) {
+        int ans =0;
+        int WCount =0;
+
+        for (int i =0; i< k; i++) {
+            if (blocks.charAt(i) == 'W') WCount++;
+        }
+        ans = WCount;
+        for (int j = k; j< blocks.length(); j++){
+            if (blocks.charAt(j) == 'W') WCount++;
+            if (blocks.charAt( j-k) == 'W') WCount--;
+
+            ans = Math.min(ans, WCount);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minimumRecolors("WBWBBBW", 2));
+
+    }
+}
