726b18e9b12ceb73793b2012b90d45fb4cab9c97
A-n-b-a-r-a-s-u <anbarasuramsame@gmail.com>
2025-03-11 16:44:40 +0530

    11-03-2025 LC Problems

---
+++ src/dailypractice/LeetCode/march/ClimbingStairs.java
@@ -0,0 +1,22 @@
+package dailypractice.LeetCode.march;
+
+public class ClimbingStairs {
+    public static int climbStairs(int n) {
+        int ans = 0;
+        if (n<=2) return n;
+
+        int prev1 =1, prev2 =2;
+        for (int i =3; i <= n; i++){
+            ans = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = ans;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(3));
+
+    }
+}
