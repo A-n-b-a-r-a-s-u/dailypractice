84279d9771123693c17c546fa15fc55ff100d03b
A-n-b-a-r-a-s-u <anbarasuramsame@gmail.com>
2025-04-03 22:23:23 +0530

    03-04-2025 LC Problems

---
+++ src/dailypractice/LeetCode/april/week1/OrderTripletTwo.java
@@ -0,0 +1,22 @@
+package dailypractice.LeetCode.april.week1;
+
+public class OrderTripletTwo {
+    public static long maximumTripletValue(int[] nums) {
+        long ans =0;
+        int maxDiff =0, k;
+        int prefixElement = nums[0];
+        for (int j=1; j< nums.length-1; j++) {
+            maxDiff = Math.max(maxDiff, prefixElement - nums[j]);
+            prefixElement = Math.max(prefixElement, nums[j]);
+            k = j+1;
+            ans = Math.max(ans, (long)maxDiff * nums[k]);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] n = {1,2,3};
+        System.out.println(maximumTripletValue(n));
+
+    }
+}
