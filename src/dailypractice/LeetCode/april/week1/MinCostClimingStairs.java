5e7b150ca2bf089d7677c5d2bf116049b06970cf
A-n-b-a-r-a-s-u <anbarasuramsame@gmail.com>
2025-04-07 14:42:58 +0530

    added the changes

--- src/dailypractice/LeetCode/april/week1/MinCostClimingStairs.java
+++ src/dailypractice/LeetCode/april/week1/MinCostClimingStairs.java
@@ -2,6 +2,22 @@ package dailypractice.LeetCode.april.week1;
 
 public class MinCostClimingStairs {
     public static int minCostClimbingStairs(int[] cost) {
+        int[] ans = new int[cost.length+1];
+        for (int i = 2; i<= cost.length; i++){
+            ans[i] = Math.min(ans[i-1] + cost[i-1], ans[i-2] + cost[i-2]);
+        }
+        return ans[cost.length];
+    }
+
+    public static void main(String[] args) {
+        int[] c = {0,2,2,1};
+        System.out.println(minCostClimbingStairs(c));
+    }
+}
+
+
+/*
+public static int minCostClimbingStairs(int[] cost) {
         int ans =0;
         for (int i = cost.length-1; i>0; i--){
             if (cost[i] == cost[i-1]){
@@ -18,8 +34,4 @@ public class MinCostClimingStairs {
         }
         return ans;
     }
-    public static void main(String[] args) {
-        int[] c = {0,2,2,1};
-        System.out.println(minCostClimbingStairs(c));
-    }
-}
+ */
