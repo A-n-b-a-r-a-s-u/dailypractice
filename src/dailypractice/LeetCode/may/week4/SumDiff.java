0feaa0bec7fbefb631e359bf78df8172c10e088e
A-n-b-a-r-a-s-u <anbarasuramsame@gmail.com>
2025-05-27 22:26:56 +0530

    May Month LC Problems

---
+++ src/dailypractice/LeetCode/may/week4/SumDiff.java
@@ -0,0 +1,19 @@
+package dailypractice.LeetCode.may.week4;
+
+public class SumDiff {
+    public static int differenceOfSums(int n, int m) {
+        int res, num1 = 0, num2 = 0;
+        for (int i =1; i<=n; i++){
+            if (i % m == 0)
+                num2 += i;
+            else
+                num1+=i;
+        }
+        res = num1 - num2;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(differenceOfSums(5, 1));;
+    }
+}
